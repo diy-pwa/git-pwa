7913aea58dfa8bb1b89b3191cc8d56c2b690b2bc
Rich Hildred <rhildred@gmail.com>
2023-05-11 12:42:20 -0400

    make git init and git pull work

--- src/GitLoader.js
+++ src/GitLoader.js
@@ -5,41 +5,43 @@ import git from 'isomorphic-git';
 import http from 'isomorphic-git/http/node/index.cjs';
 import parseArgs from 'minimist';
 
-export default class{
-    constructor(init)
-    {
+export default class {
+    constructor(init) {
         if (typeof (init) != "undefined") {
             Object.assign(this, init);
-        }else{
+        } else {
             this.argv = parseArgs(process.argv);
         }
-        this.config = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
+        try {
+            this.config = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
+        } catch {
+            this.config = {};
+        }
         this.base = {
             gitdir: '.git',
+            dir: ".",
             fs: fs,
             http
         }
         this.base.corsProxy = 'https://corsproxy-dqo.pages.dev/gitcorsproxy';
-        if(this.config && this.config.http && this.config.http.corsProxy){
+        if (this.config && this.config.http && this.config.http.corsProxy) {
             this.base.corsProxy = this.config.http.corsProxy
         }
         this.commandData = {
             clone: {
-                    url: this.argv._[3],
-                    ref: this.argv.b || this.argv.branch ||'main',
-                    singleBranch: true,
-                    depth: 10,
-                    dir: this.argv._[4] || path.basename(sUrl, ".git")
-            },
-            pull: {
-                    dir: "."
+                url: this.argv._[3],
+                ref: this.argv.b || this.argv.branch || 'main',
+                singleBranch: true,
+                depth: 10,
+                dir: this.argv._[4] || path.basename(this.argv._[3] || "", ".git"),
+                gitdir: undefined
             }
         }
     }
-    async runCommand(){
+    async runCommand() {
         let oConfig = {};
         Object.assign(oConfig, this.base);
-        if(typeof(this.commandData[this.argv._[2]]) != "undefined"){
+        if (typeof (this.commandData[this.argv._[2]]) != "undefined") {
             Object.assign(oConfig, this.commandData[this.argv._[2]]);
         }
         await git[this.argv._[2]](oConfig);
