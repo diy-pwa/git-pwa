4b460e63eea7987bd7b6e47e2fe6f5e32cd4d611
Rich Hildred <rhildred@wlu.ca>
2023-05-10 20:58:28 -0400

    handle case where command is missed

--- git.js
+++ git.js
@@ -3,7 +3,7 @@
 import GitLoader from './src/GitLoader.js';
 import ora from 'ora';
 
-const spinner = ora(`running git ${process.argv[2]} ... `).start();
+const spinner = ora(`running git ${process.argv[2] || ""} ... `).start();
 
 const oLoader = new GitLoader();
 
