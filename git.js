216a99eec284c24ebf18c18237c84ce1fabe14b3
Rich Hildred <rhildred@wlu.ca>
2023-05-14 12:29:05 -0400

    start git deploy

--- git.js
+++ git.js
@@ -3,12 +3,20 @@
 import GitLoader from './src/GitLoader.js';
 import ora from 'ora';
 
-const spinner = ora(`running git ${process.argv[2] || ""} ... `).start();
+const spinner = ora(`running git ${process.argv[2] || ''} ... `).start();
 
 const oLoader = new GitLoader();
 
-oLoader.runCommand().catch((e) => {
+oLoader
+  .runCommand()
+  .then((e) => {
+    if (typeof e != 'undefined') {
+      console.log(e);
+    }
+  })
+  .catch((e) => {
     console.log(e.toString());
-}).finally(()=>{
+  })
+  .finally(() => {
     spinner.stop();
-});
+  });
