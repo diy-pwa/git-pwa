5c73bf0c67a327802b564f65528dc639301c6a37
Rich Hildred <rhildred@wlu.ca>
2023-05-14 14:03:39 -0400

    move thjis.command checking before git to build on commands

--- src/GitLoader.js
+++ src/GitLoader.js
@@ -6,56 +6,59 @@ import http from 'isomorphic-git/http/node/index.cjs';
 import parseArgs from 'minimist';
 
 export default class {
-  constructor(init) {
-    if (typeof init != 'undefined') {
-      Object.assign(this, init);
-    } else {
-      this.argv = parseArgs(process.argv);
+    constructor(init) {
+        if (typeof init != 'undefined') {
+            Object.assign(this, init);
+        } else {
+            this.argv = parseArgs(process.argv);
+        }
+        try {
+            this.config = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
+        } catch {
+            this.config = {};
+        }
+        this.base = {
+            gitdir: '.git',
+            dir: '.',
+            fs: fs,
+            http,
+        };
+        this.base.corsProxy = 'https://corsproxy-dqo.pages.dev/gitcorsproxy';
+        if (this.config && this.config.http && this.config.http.corsProxy) {
+            this.base.corsProxy = this.config.http.corsProxy;
+        }
+        this.commandData = {
+            clone: {
+                url: this.argv._[3],
+                ref: this.argv.b || this.argv.branch || 'main',
+                singleBranch: true,
+                depth: 10,
+                dir: this.argv._[4] || path.basename(this.argv._[3] || '', '.git'),
+                gitdir: undefined,
+            },
+        };
+        this.command = {
+            deploy: () => {
+                return 'deployed';
+            },
+            status: () => {
+                return 'want to fix this for bare `git status`'
+            }
+        };
     }
-    try {
-      this.config = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
-    } catch {
-      this.config = {};
+    async runCommand() {
+        let oConfig = {};
+        Object.assign(oConfig, this.base);
+        if (typeof this.commandData[this.argv._[2]] != 'undefined') {
+            Object.assign(oConfig, this.commandData[this.argv._[2]]);
+        }
+        if (typeof this.command[this.argv._[2]] != 'undefined') {
+            // add new command
+            return await this.command[this.argv._[2]]();
+        } else if (typeof git[this.argv._[2]] != 'undefined') {
+            return await git[this.argv._[2]](oConfig);
+        } else {
+            throw new Error('unimplemented');
+        }
     }
-    this.base = {
-      gitdir: '.git',
-      dir: '.',
-      fs: fs,
-      http,
-    };
-    this.base.corsProxy = 'https://corsproxy-dqo.pages.dev/gitcorsproxy';
-    if (this.config && this.config.http && this.config.http.corsProxy) {
-      this.base.corsProxy = this.config.http.corsProxy;
-    }
-    this.commandData = {
-      clone: {
-        url: this.argv._[3],
-        ref: this.argv.b || this.argv.branch || 'main',
-        singleBranch: true,
-        depth: 10,
-        dir: this.argv._[4] || path.basename(this.argv._[3] || '', '.git'),
-        gitdir: undefined,
-      },
-    };
-    this.command = {
-      deploy: () => {
-        return 'deployed';
-      },
-    };
-  }
-  async runCommand() {
-    let oConfig = {};
-    Object.assign(oConfig, this.base);
-    if (typeof this.commandData[this.argv._[2]] != 'undefined') {
-      Object.assign(oConfig, this.commandData[this.argv._[2]]);
-    }
-    if (typeof git[this.argv._[2]] != 'undefined') {
-      return await git[this.argv._[2]](oConfig);
-    } else if (typeof this.command[this.argv._[2]] != 'undefined') {
-      // add new command
-      return await this.command[this.argv._[2]]();
-    } else {
-      throw new Error('unimplemented');
-    }
-  }
 }
