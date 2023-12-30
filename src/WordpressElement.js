4492ba7bf6e4e50a653127e5a018b10d4a939882
Rich Hildred <rhildred@gmail.com>
2023-12-29 23:47:27 -0500

    add style to iframe so it fills the available space

--- src/WordpressElement.js
+++ src/WordpressElement.js
@@ -5,7 +5,7 @@ class WordpressElement extends HTMLElement{
         super();
     }
     async connectedCallback(){
-        this.innerHTML = `<iframe id="wordpress" credentialless></iframe>`;
+        this.innerHTML = `<iframe id="wordpress" credentialless style="width:100%;height:100vh"></iframe>`;
         const client = await startPlaygroundWeb({
          iframe: document.querySelector('iframe#wordpress'),
          remoteUrl: "https://playground.wordpress.net/remote.html" }
