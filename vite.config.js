2a222626c92682b1613c14ebc4f5cd4ca0c36979
Rich Hildred <rhildred@wlu.ca>
2024-02-03 14:30:24 -0500

    missed building promises

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(
     ],
     build: {
       lib: {
-        entry: resolve(__dirname, 'src/promises.js'),
+        entry: resolve(__dirname, 'src/WebContainerPromises.js'),
         name:  "webcontainer-fs-promises"
       }
     },
